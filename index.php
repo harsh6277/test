9fc4bbbde1370d2a9b867df85369f8a021faac68
harsh6277 <harsh6277@gmail.com>
2017-03-03 17:27:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
   <head>
   </head>
   <body>
-    <h1>my first code in github</h1>
+    <?php
+    $name ="harsh";
+    if($name == "harsh"){
+      echo "<h1>my first code in github</h1>";
+    }else{
+      echo "<h1>ERROR</h1>";
+    }
+      
+    ?>
+    
   </body>
 </html>
